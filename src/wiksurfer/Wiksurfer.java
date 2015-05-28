cbd36c0ab0ba31e7a38cbba83732252935c8708b
xtfkpi <xtfkpi@xtfkpi-Lenovo-E10-30>
2015-05-28 23:13:16 +0300

    it fucking works

--- src/wiksurfer/Wiksurfer.java
+++ src/wiksurfer/Wiksurfer.java
@@ -9,18 +9,17 @@ public class Wiksurfer {
         UserAgent userAgent = new UserAgent();
         userAgent.settings.autoSaveAsHTML = true;  //change settings to autosave last visited page.
         Elements elements;
-        //System.out.println("SETTINGS:\n" + userAgent.settings);   
+        
         try {
-
             String href = "http://en.wiktionary.org/wiki/Category:English_uncountable_nouns";
 
-            for (i = 0; i < 1; i++) {
+            for (i = 0; i < 70; i++) {
                 userAgent.visit(href);
-                href = userAgent.doc.findFirst("<a title>next page").getAt("href");
-
+                href = userAgent.doc.getHyperlink("next page").getHref().replaceAll("amp;", "");
                 System.out.println("next page:" + href);
+
                 elements = userAgent.doc.findEvery("<div class=mw-category-group>").findEach("<li>").findEach("<a>");                        //find all non-nested divs
-                System.out.println("Each a: " + elements.size() + elements);
+                //System.out.println("Each a: " + elements.size() + elements);
                 for (Element el : elements) {                                     //iterate through Results
                     System.out.println(el.getText());         //print each element and its contents
                 }
